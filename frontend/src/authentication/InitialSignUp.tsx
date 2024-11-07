721def2a603b9eef0ddccd9236e89c03f074800d
izuchukwumba <barth.poetic@gmail.com>
2024-11-07 02:35:50 -0600

    --

--- frontend/src/authentication/InitialSignUp.tsx
+++ frontend/src/authentication/InitialSignUp.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
-import SelectT from "./SelectT";
+import SelectT from "./SelectCollege";
 
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState<string>("");
